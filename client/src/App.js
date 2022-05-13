425b2d352574df201b6edbc4461f055279cc77b9
goffygoo <jrvineetoli52@gmail.com>
2022-05-14 02:22:34 +0530

    router added

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
 import { Provider } from "react-redux"
 import store from "./store";
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <Provider store={store}>
-      <div className="App">
-        OP bhai OP
-      </div>
+      <Router>
+        <Routes>
+          <Route exact path="/login" element={<Login/>} />
+          <Route exact path="/home" element={<Home/>} />
+        </Routes>
+      </Router>
     </Provider>
   );
 }
