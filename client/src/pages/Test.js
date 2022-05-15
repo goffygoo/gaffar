f820d682dbbc35ada6c64eb6e945e7b2b02c14ef
goffygoo <jrvineetoli52@gmail.com>
2022-05-15 21:38:19 +0530

    redux added

--- client/src/pages/Test.js
+++ client/src/pages/Test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
-import { Navigate, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { bindActionCreators } from 'redux'
 
 import * as actionTest from '../action/actionTest'
